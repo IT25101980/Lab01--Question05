e6396a35ffffbb82bc2225919e7cf1ac06a95e5f
IT25101980 <it25101980@my.sliit.lk>
2026-02-06 23:07:19 +0530

    Lab01-Question05

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main{
+	
+	public static void main(String[] args){
+		
+		Calculator cal = new Calculator();
+		
+		int exp1;
+		int exp2;
+		
+		exp1 = cal.square(cal.add(cal.multiply(3,4),cal.multiply(5,7)));
+		
+		System.out.println("Result of (3 * 4 + 5 * 7)^2 = " + exp1);
+		 
+		exp2 = cal.add(cal.square(cal.add(4,7)),cal.square(cal.add(8,3)));
+		
+		System.out.println("Result of (4 + 7)^2 + (8 + 3)^2 = " + exp2);
+	}
+}
